5752194666e0db452534459ad25c123d9f6ac903
Davo-source <95258179+Davo-source@users.noreply.github.com>
2024-03-04 07:15:31 -0600

    Delete convertions/convertions directory

--- convertions/convertions/src/main/java/com/david/oramas/convertions/entity/RuleActionData.java
+++
@@ -1,9 +0,0 @@
-package com.david.oramas.convertions.entity;
-
-import lombok.Data;
-
-@Data
-public class RuleActionData {
-    private String name;
-    private int sequence;
-}
--- convertions/convertions/src/main/java/com/david/oramas/convertions/service/RuleDataService.java
+++
@@ -1,8 +0,0 @@
-package com.david.oramas.convertions.service;
-
-
-import com.david.oramas.convertions.entity.Data;
-
-public interface RuleDataService {
-    Data addRuleData(Data data);
-}
